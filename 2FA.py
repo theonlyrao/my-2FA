c4f65ac4b0a706d9146ba15a087c8e23c2800f26
Ashwin Rao <ashwin@csc-vm-2>
2021-02-27 17:20:35 -0500

    Complete login user except for updating token

--- 2FA.py
+++ 2FA.py
@@ -4,19 +4,20 @@ import crypt
 import os
 
 
-# Login
-def quit_if_user_exists(username):
+# Common
+def user_exists(username):
+    exists = False
     with open('/etc/shadow', 'r') as fp:
-        arr = []
         for line in fp:
             temp = line.split(':')
             if temp[0] == username:
-                click.echo("FAILURE: user %s already exists")
-                sys.exit()
+                exists = True
+    return exists
 
 
+# Login
 def update_etc_shadow(username, password, salt, initial_token):
-    hashed = crypt.crypt(password + initial_token, salt)
+    hashed = crypt.crypt(password + initial_token, '$6$' + salt)
     line = username + ':' + hashed + ":17710:0:99999:7:::"
     file = open("/etc/shadow", "a+")
     file.write(line + '\n')
@@ -28,7 +29,7 @@ def create_home_dir(username):
     try:
         os.mkdir("/home/" + username)
     except Exception:
-        click.echo("Directory: /home/" + username + " already exists")
+        click.echo("Directory: /home/" + username + " already exists", err=True)
 
 
 def update_etc_passwd(username):
@@ -50,23 +51,67 @@ def update_etc_passwd(username):
 @click.command()
 @click.option(
     '--username',
-    prompt="Please enter the username for this new user: ")
+    prompt="Please enter the username for this new user")
 @click.option(
     '--password',
-    prompt="Please enter the password for this new user: ")
+    prompt="Please enter the password for this new user")
 @click.option(
     '--salt',
-    prompt="Please enter the salt for this new user: ")
+    prompt="Please enter the salt for this new user")
 @click.option(
     '--initial_token',
-    prompt="Please enter the initial token for this new user: ")
+    prompt="Please enter the initial token for this new user")
 def create_user(username, password, salt, initial_token):
-    quit_if_user_exists(username)
+    if user_exists(username):
+        click.echo("FAILURE: user %s exists" % username, err=True)
+        sys.exit()
+
     update_etc_shadow(username, password, salt, initial_token)
     create_home_dir(username)
     update_etc_passwd(username)
     click.echo("SUCCESS: %s created" % username)
-    pass
+
+
+# Login
+def perform_authentication(username, password, current_token, next_token):
+    with open('/etc/shadow', 'r') as fp:
+        for line in fp:
+            temp = line.split(':')
+            if temp[0] == username:  # checking whether entered username exist or not
+                salt_and_pass = (temp[1].split('$'))  # retrieving salt against the user
+                salt = salt_and_pass[2]
+                result = crypt.crypt(password + current_token,
+                                     '$6$' + salt)  # calculating hash via salt and password entered by user
+                if result == temp[1]:  # comparing generated salt with existing salt entery
+                    click.echo("SUCCESS: Login Successful")
+                    return
+                else:
+                    click.echo("FAILURE: either passwd or token incorrect", err=True)
+                    sys.exit()
+    click.echo("FAILURE: user %s does not exist" % username, err=True)
+    sys.exit()
+
+
+@click.command()
+@click.option(
+    '--username',
+    prompt="Please enter your username",
+)
+@click.option(
+    '--password',
+    prompt="Please enter your password")
+@click.option(
+    '--current_token',
+    prompt="Please enter your current token")
+@click.option(
+    '--next_token',
+    prompt="Please enter the next token")
+def login_user(username, password, current_token, next_token):
+    if not user_exists(username):
+        click.echo("FAILURE: user %s does not exist" % username, err=True)
+        sys.exit()
+
+    perform_authentication(username, password, current_token, next_token)
 
 
 @click.command()
@@ -80,9 +125,10 @@ def create_user(username, password, salt, initial_token):
 def main(action):
     # noinspection PyBroadException
     try:
-        click.echo('Hello %s!' % action)
         if int(action) == 1:
             create_user()
+        if int(action) == 2:
+            login_user()
         else:
             click.echo("Not implemented.", err=True)
     except Exception:
